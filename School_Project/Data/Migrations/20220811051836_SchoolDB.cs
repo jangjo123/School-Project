9008fba0fa61e5c0a575e9999a3868c855a2d423
jangjo123 <cjkh1324@naver.com>
2022-08-11 15:46:22 +0900

    css style에 적용

---
+++ School_Project/Data/Migrations/20220811051836_SchoolDB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace School_Project.Data.Migrations
+{
+    public partial class SchoolDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
