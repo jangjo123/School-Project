d9d7e6501109b989213af688db00dba2d7e2033b
jangjo123 <cjkh1324@naver.com>
2022-11-19 18:59:51 +0900

    django Dockerfile

--- School_Project/Pages/js/main.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector('.navbar_toogleBtn')
-const menu_right = document.querySelector('.navbar_right')
-
-toggleBtn.addEventListener('click', () => {
-    menu_right.classList.toggle('active');
-});
